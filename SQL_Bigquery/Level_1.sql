f6551370882aad2e3bf52d164e8fe97c1cb48a64
abbyy721 <46148055+abbyy721@users.noreply.github.com>
2025-08-02 14:38:34 -0500

    Update Level_1.sql

--- SQL_Bigquery/Level_1.sql
+++ SQL_Bigquery/Level_1.sql
@@ -1,7 +1,21 @@
-SELECT, FROM, WHERE
-AS
-GROUP BY 
-COUNT, AVG, MIN 
-HAVING COUNT 
-ORDER BY
-LIMIT
+# Level 1 – Basic SQL Queries
+
+## 📘 Table Reference: `trainer`
+
+| column                 | type   | description                            |
+|------------------------|--------|----------------------------------------|
+| id                     | INT    | Unique trainer ID                      |
+| name                   | STRING | Trainer name                           |
+| age                    | INT    | Trainer's age                          |
+| hometown               | STRING | City the trainer is from               |
+| preferred_pokemon_type | STRING | Favorite Pokémon type (e.g. Bug, Grass)|
+| badge_count            | INT    | Number of badges earned                |
+| achievement_level      | STRING | Level: Beginner / Intermediate / etc.  |
+
+---
+
+## 🟡 Question 1
+1. Write an SQL query to display all data from the trainer table. 
+```sql
+SELECT *
+FROM `Basic.trainer`
