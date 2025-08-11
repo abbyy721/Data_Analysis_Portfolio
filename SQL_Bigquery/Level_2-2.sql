5adc46a0f53fd4a0b2c53f7b40eac0658e89b90e
abbyy721 <46148055+abbyy721@users.noreply.github.com>
2025-08-11 16:03:07 -0500

    Create Level_2-2.sql

---
+++ SQL_Bigquery/Level_2-2.sql
@@ -0,0 +1,63 @@
+/* 
+
+🔹 Level 2 – Advanced SQL Queries
+🔸 Skills used : 
+- STRING FUNCTIONS (CONCAT, SPLIT, REPLACE, TRIM, UPPER)
+- DATE/TIME FUNCTIONS (TIMESTAMP, DATETIME, EXTRACT, TRUNC, PARSE, FORMAT, LAST_DAY, DATETIME_DIFF )
+
+
+📘 Table Reference (1) : `trainer`
+| column                 | type   | description                            |
+|------------------------|--------|----------------------------------------|
+| id                     | INT    | Unique trainer ID                      |
+| name                   | STRING | Trainer name                           |
+| age                    | INT    | Trainer's age                          |
+| hometown               | STRING | City the trainer is from               |
+| preferred_pokemon_type | STRING | Favorite Pokémon type (e.g. Bug, Grass)|
+| badge_count            | INT    | Number of badges earned                |
+| achievement_level      | STRING | Level: Beginner / Intermediate / etc.  |
+
+📘 Table Reference (2) : `pokemon` 
+| column            | type    | description                         |
+|-------------------|---------|-------------------------------------|
+| id                | INT     | Unique Pokémon ID                   |
+| kor_name          | STRING  | Pokémon name in Korean              |
+| eng_name          | STRING  | Pokémon name in English             |
+| type1             | STRING  | Primary type (e.g. Grass, Fire)     |
+| type2             | STRING  | Secondary type (nullable)           |
+| total             | INT     | Total base stats                    |
+| hp                | INT     | HP stat                             |
+| attack            | INT     | Attack stat                         |
+| defense           | INT     | Defense stat                        |
+| special_attack    | INT     | Special Attack stat                 |
+| special_defense   | INT     | Special Defense stat                |
+| speed             | INT     | Speed stat                          |
+| generation        | INT     | Generation number                   |
+| is_legendary      | BOOLEAN | Whether the Pokémon is legendary    | 
+
+-- 📘 Table Reference (3): `trainer_pokemon`
+
+-- | column           | type     | description                                     |
+-- |------------------|----------|-------------------------------------------------|
+-- | id               | INT      | Unique ID for each trainer's Pokémon entry     |
+-- | trainer_id       | INT      | Foreign key referencing the trainer            |
+-- | pokemon_id       | INT      | Foreign key referencing the Pokémon            |
+-- | level            | INT      | Pokémon's current level                        |
+-- | experience_point | INT      | Accumulated experience points                  |
+-- | current_health   | INT      | Pokémon's current HP                           |
+-- | catch_date       | DATE     | The date the Pokémon was caught                |
+-- | catch_datetime   | DATETIME | Full timestamp of when the Pokémon was caught  |
+-- | location         | STRING   | Location where the Pokémon was caught          |
+-- | status           | STRING   | Status of the Pokémon (e.g., Active, Inactive) |*/
+
+
+--------------------------------------------------------------------------------------------------------------------------
+
+
+📄 Questions
+
+  
+/*Based on the trainer's Pokémon catch date (catch_date), 
+calculate the number of Pokémon caught in January 2023. */
+
+  
